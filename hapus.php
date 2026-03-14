39c7fdb08c05c706b8271dd30bf52faeb9306fc8
Muad-syntax <mohammad.suaebummuad9@smk.belajar.id>
2026-03-14 16:27:51 +0700

    update delete

---
+++ hapus.php
@@ -0,0 +1,8 @@
+<?php
+include "koneksi.php";
+$id = $_POST['id'];
+$query = "DELETE FROM tbsiswa WHERE id = '$id'";
+$hasil = mysqli_query($koneksi, $query);
+
+header("Location: index.php")
+?>
--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 include "koneksi.php";
-
+if (isset($_GET['id'])) {
+    $get = $_GET['id'];
+    $query = "SELECT * FROM tbsiswa WHERE id = '$get' or nama = '$get' or kelas = '$get'";
+} else {
+    $query = "SELECT * FROM tbsiswa ";
+}
+$hasil = mysqli_query($koneksi, $query);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,17 +19,29 @@ include "koneksi.php";
 
 <body>
     <h2>Data User</h2>
+    <form action="" method="get">
+        <input type="text" name="id" required>
+        <button>cari</button>
+    </form>
     <table border="1" cellpadding="8" cellspacing="0" class="table">
         <tr>
             <th>ID</th>
             <th>Nama</th>
             <th>Kelas</th>
+            <th>Aksi</th>
         </tr>
         <?php while ($row = mysqli_fetch_assoc($hasil)) { ?>
             <tr>
                 <td><?php echo $row['id']; ?></td>
                 <td><?php echo $row['nama']; ?></td>
                 <td><?php echo $row['kelas']; ?></td>
+                <!-- <td><a href="hapus.php?id=<?php echo $row['id']; ?>">hapus</a></td> -->
+                <td>
+                    <form action="hapus.php" method="post">
+                        <input type="hidden" name="id" value="<?php echo $row['id']; ?>">
+                        <button>hapus</button>
+                    </form>
+                </td>
             </tr>
         <?php } ?>
 </body>
